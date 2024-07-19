c4b3f057912f939d9978184599c489b0ea16933e
Uchechukwu Orji <orjiuchechukwu52@yahoo.com>
2024-07-19 14:02:42 +0100

    refactor: close redis connection on application shutdown

--- courageous_comets/__main__.py
+++ courageous_comets/__main__.py
@@ -1,1 +1,27 @@
-# Placeholder - run the main program here
+import asyncio
+
+from redis.asyncio import Redis
+
+from courageous_comets import logger
+from courageous_comets.settings import Settings
+
+
+async def main() -> None:
+    """Start the appication."""
+    logger.debug("connecting to Redis")
+    redis = Redis(
+        host=Settings.REDIS_HOST,
+        port=Settings.REDIS_PORT,
+        password=Settings.REDIS_PASSWORD,
+    )
+    logger.debug("checking Redis connection.")
+    await redis.ping()
+    try:
+        logger.debug("starting bot.")
+    finally:
+        logger.debug("closing Redis connection.")
+        await redis.aclose()
+        logger.debug("application shutdown complete.")
+
+
+asyncio.run(main())
--- courageous_comets/redis/__init__.py
+++
@@ -1,9 +0,0 @@
-from redis.asyncio import Redis
-
-from courageous_comets.settings import Settings
-
-redis = Redis(
-    host=Settings.REDIS_HOST,
-    port=Settings.REDIS_PORT,
-    password=Settings.REDIS_PASSWORD,
-)
