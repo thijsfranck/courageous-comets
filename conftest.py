4821702c7f9287c830b269655a6a83c9ceaafd6a
thijsfranck <thijsfranck@gmail.com>
2024-07-20 16:10:40 +0000

    ci: move loading of nltk data to conftest.py

---
+++ conftest.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+
+import pytest
+import yaml
+
+
+@pytest.fixture(scope="session")
+def application_config() -> dict:
+    """Load the application configuration for testing."""
+    with Path("application.yaml").open("r") as file:
+        return yaml.safe_load(file)
+
+
+@pytest.fixture(scope="session")
+def _load_nltk_data(application_config: dict) -> None:
+    """Load the NLTK data for testing."""
+    import nltk
+
+    resources = application_config.get("nltk", [])
+
+    for resource in resources:
+        nltk.download(resource, quiet=True)
