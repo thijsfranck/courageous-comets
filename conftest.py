13ed85b9f81d75f4ce9fa3e16eeb60aebef8344b
thijsfranck <thijsfranck@gmail.com>
2024-07-20 19:23:24 +0200

    feat: add sentiment analysis (#24)

---
+++ conftest.py
@@ -0,0 +1,26 @@
+import os
+from pathlib import Path
+
+import nltk
+import pytest
+import yaml
+
+
+@pytest.fixture(scope="session")
+def application_config() -> dict:
+    """Load the application configuration for testing."""
+    with Path("application.yaml").open("r") as file:
+        return yaml.safe_load(file)
+
+
+@pytest.fixture(scope="session", autouse=True)
+def _load_nltk_data(application_config: dict) -> None:
+    """Load the NLTK data for testing."""
+    resources = application_config.get("nltk", [])
+
+    for resource in resources:
+        nltk.download(
+            resource,
+            quiet=True,
+            download_dir=os.getenv("NLTK_DATA", "nltk_data"),
+        )
