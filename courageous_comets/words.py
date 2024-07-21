f242b604a4e60fa0b1b0dd3476cc909d203766fd
thijsfranck <thijsfranck@gmail.com>
2024-07-21 10:01:18 +0200

    fix: handle contractions in tokenizer (#26)

--- courageous_comets/words.py
+++ courageous_comets/words.py
@@ -1,25 +1,63 @@
-from collections import defaultdict
+from collections import Counter
 
+import contractions
 from nltk.stem.snowball import SnowballStemmer, stopwords
 from nltk.tokenize import word_tokenize
 
-STOP_WORDS = set(stopwords.words("english"))
-
 
 def tokenize_sentence(sentence: str) -> list[str]:
-    """Split a sentence into tokens.
-
-    The tokenizer applies stemming to the words that make up the sentence
-    using the Snowball stemmer and removes stopwords.
     """
+    Split a sentence into tokens.
+
+    The tokenizer applies the following steps:
+
+    - Expand contractions.
+    - Break the sentence into words.
+    - Stem the words.
+    - Remove stopwords and words with a length of 1.
+
+    Parameters
+    ----------
+    sentence : str
+        The sentence to tokenize.
+
+    Returns
+    -------
+    list[str]
+        The tokens derived from the sentence.
+
+    Notes
+    -----
+    The tokenizer is intended to be used with English text.
+    """
+    # Expand contractions
+    expanded = contractions.fix(sentence)
+
+    # Break the sentence into words
+    words = word_tokenize(expanded)
+
+    # Stem the words
     stemmer = SnowballStemmer("english")
-    stemmed_words = [stemmer.stem(token) for token in word_tokenize(sentence)]
-    return [word for word in stemmed_words if len(word) > 1 and word not in STOP_WORDS]
+    stemmed_words = [stemmer.stem(word) for word in words]
+
+    # Remove stopwords and words with a length of 1
+    stop_words = set(stopwords.words("english"))
+
+    return [word for word in stemmed_words if len(word) > 1 and word not in stop_words]
 
 
 def word_frequency(words: list[str]) -> dict[str, int]:
-    """Count the number of times each word appears in words."""
-    frequency: dict[str, int] = defaultdict(int)
-    for word in words:
-        frequency[word] += 1
-    return frequency
+    """
+    Count the number of times each word appears in the given list of words.
+
+    Parameters
+    ----------
+    words : list[str]
+        The list of words to count the frequency of.
+
+    Returns
+    -------
+    dict[str, int]
+        The frequency of each word in words.
+    """
+    return Counter(words)
