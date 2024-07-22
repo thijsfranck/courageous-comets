afde5e273902422b9d64419dda60f6f0287ffd12
isaa_ctaylor <isaac@isaactaylor.xyz>
2024-07-22 12:10:25 +0100

    refactor: include save_message in Messages cog (#34)
    Co-authored-by: thijsfranck <thijsfranck@gmail.com>

--- courageous_comets/cogs/messages.py
+++ courageous_comets/cogs/messages.py
@@ -2,7 +2,6 @@ import logging
 
 import discord
 from discord.ext import commands
-from redis.asyncio import Redis
 
 from courageous_comets.client import CourageousCometsBot
 from courageous_comets.models import VectorizedMessage
@@ -22,9 +21,20 @@ class Messages(commands.Cog):
     @commands.Cog.listener(name="on_message")
     async def on_message(self, message: discord.Message) -> None:
         """
-        Save a message to the database.
+        When a message is received, save it to Redis.
 
-        If the bot is not connected to Redis, this method does nothing.
+        Parameters
+        ----------
+        message : discord.Message
+            The message to save.
+        """
+        await self.save_message(message)
+
+    async def save_message(self, message: discord.Message) -> None:
+        """
+        Save a message on Redis.
+
+        Ignore messages that are not in a guild or if the bot is not connected to Redis.
 
         Parameters
         ----------
@@ -32,9 +42,36 @@ class Messages(commands.Cog):
             The message to save.
         """
         if not self.bot.redis:
-            return
+            return logger.warning(
+                "Ignoring message %s because the bot is not connected to Redis",
+                message.id,
+            )
 
-        await save_message(message, self.bot.redis, self.vectorizer)
+        if not message.guild:
+            return logger.debug(
+                "Ignoring message %s because it's not in a guild",
+                message.id,
+            )
+
+        embedding = await self.vectorizer.embed(message.content)
+
+        vectorized_message = VectorizedMessage(
+            user_id=str(message.author.id),
+            message_id=str(message.id),
+            channel_id=str(message.channel.id),
+            guild_id=str(message.guild.id),
+            content=message.content,
+            timestamp=message.created_at,
+            embedding=embedding,
+        )
+
+        key = await messages.save_message(self.bot.redis, vectorized_message)
+
+        return logger.info(
+            "Saved message %s to Redis with key %s",
+            message.id,
+            key,
+        )
 
 
 async def setup(bot: CourageousCometsBot) -> None:
@@ -47,41 +84,3 @@ async def setup(bot: CourageousCometsBot) -> None:
         The bot instance.
     """
     await bot.add_cog(Messages(bot))
-
-
-async def save_message(message: discord.Message, redis: Redis, vectorizer: Vectorizer) -> None:
-    """
-    Save a message on Redis.
-
-    Parameters
-    ----------
-    message : discord.Message
-        The message to save.
-    redis: Redis
-        The Redis connection instance.
-    vectorizer : Vectorizer
-        The vectorizer to use to embed the message.
-
-    Returns
-    -------
-    str
-        The key to the data on Redis.
-    """
-    if not message.guild:
-        return logger.debug("Ignoring message %s because it's not in a guild", message.id)
-
-    embedding = await vectorizer.embed(message.content)
-
-    vectorized_message = VectorizedMessage(
-        user_id=str(message.author.id),
-        message_id=str(message.id),
-        channel_id=str(message.channel.id),
-        guild_id=str(message.guild.id),
-        content=message.content,
-        timestamp=message.created_at,
-        embedding=embedding,
-    )
-
-    key = await messages.save_message(redis, vectorized_message)
-
-    return logger.info("Saved message %s to Redis with key %s", message.id, key)
