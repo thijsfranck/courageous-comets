7c5ce5f2edec229a75b98344d37b186bb7bf8fef
thijsfranck <thijsfranck@gmail.com>
2024-07-25 18:12:01 +0000

    fix: handle PackageNotFoundError

--- courageous_comets/__init__.py
+++ courageous_comets/__init__.py
@@ -1,6 +1,12 @@
 import importlib.metadata
+import logging
 
 from .client import bot
 
 __all__ = ["bot"]
-__version__ = importlib.metadata.version("courageous_comets")
+
+try:
+    __version__ = importlib.metadata.version("courageous_comets")
+except importlib.metadata.PackageNotFoundError:
+    logging.warning("Could not determine the package version.")
+    __version__ = "unknown"
